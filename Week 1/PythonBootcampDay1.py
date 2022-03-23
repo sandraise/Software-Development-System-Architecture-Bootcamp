2109c34a3626caea578be0969343471a2703644b
sandraise <sandraise@hotmail.co.uk>
2022-03-23 20:41:24 +0000

    Initial commit

---
+++ Week 1/PythonBootcampDay1.py
@@ -0,0 +1,14 @@
+# Creating a variable called name
+name = "Sandra"
+
+# Creating a variable that contains your interests
+interests = "I am interested in Agile Delivery, specifically SaaS. I am also interested in SQL."
+
+# Print name variable to the terminal
+print(name)
+
+# Print interests variable to the terminal
+print(interests)
+
+# Print name and interests variable to the terminal
+print(name,interests)
