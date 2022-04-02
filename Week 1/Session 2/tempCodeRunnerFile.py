5eb280a9d4de7644dd3f483d063b30ee53d26c0f
sandraise <sandraise@hotmail.co.uk>
2022-04-02 18:32:55 +0100

    Updated files

--- Week 1/Session 2/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-input1 = int(input("Enter a the first number: "))
-input2 = int(input("Enter a the second number: "))
-input3 = int(input("Enter a the third number: "))
-input4 = int(input("Enter a the fourth number: "))
-input5 = int(input("Enter a the fifth number: "))
-
-print(input1,input2,input3,input4,input5, sep="   ")
